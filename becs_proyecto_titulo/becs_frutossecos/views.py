4245275ea64f8449a12f999a5e90e3fe8829a596
Valentina González <135192416+poisonpinkk@users.noreply.github.com>
2025-04-12 01:18:06 -0400

    intento de inicio de sesion

--- becs_proyecto_titulo/becs_frutossecos/views.py
+++ becs_proyecto_titulo/becs_frutossecos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.contrib.auth.forms import AuthenticationForm
+
+def login_view(request):
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 # Create your views here.
